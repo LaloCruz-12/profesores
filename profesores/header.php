d2fa5b60cf3b11cfd11bf50a93d813dfdb9dd1c4
LaloCruz-12 <77806087+LaloCruz-12@users.noreply.github.com>
2021-05-07 18:37:34 -0500

    Add files via upload

---
+++ profesores/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cedula</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styles.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+  </head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-light"> <!--Barra Principal-->
+      <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="true" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="row-md-6">
+          <a class="navbar-brand" href="inicio.html">Inicio</a>
+          <a class="navbar-brand" href="cedula.html">Cedula</a>
+          <a class="navbar-brand" href="cedulaForm.html">Editar</a>
+        </div>
+        <div class="row-md-6">
+           <a class="navbar-brand" href="login_2.html">Salir</a>
+        </div>
+      </div>
+    </nav> <!-- Fin Barra Principal-->
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+  
+</body>
